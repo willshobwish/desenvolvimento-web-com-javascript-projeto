7617da59386ced5718fd92b89ea8903c0b792232
willshobwish <willianyos13@gmail.com>
2022-02-06 22:00:34 -0300

    Modificar e alterar os comentários

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -1,29 +1,39 @@
+// A page can't be manipulated safely until the document is "ready." jQuery detects this state of readiness for you. https://learn.jquery.com/using-jquery-core/document-ready/
 $(document).ready(function () {
-    console.log("Ready for use!");
+    console.log("ready!");
 });
 
+// Criacao dos menus em js, ficara mais facil caso tenha alguma alteracao nos botoes do menu, assim, alterara simultaneamente as quatro paginas.
+// Index
+$("#index") //Seleciona a id do div
+    .append(
+        '<a href="./projeto/pages/page1-posts.html" id="posts" class="menu-button">Posts</a>'
+    )
+    .append(
+        '<a href="./projeto/pages/page2-projetos.html" id="projetos" class="menu-button">Projetos</a>'
+    )
+    .append(
+        '<a href="./projeto/pages/page3-contato.html" id="contato" class="menu-button">Contato</a>'
+    );
+
+// Restante da pagina
+$("#page") //Seleciona a id do div
+    .append(
+        '<a href="./page1-posts.html" id="posts" class="menu-button">Posts</a>'
+    )
+    .append(
+        '<a href="./page2-projetos.html" id="projetos" class="menu-button">Projetos</a>'
+    )
+    .append(
+        '<a href="./page3-contato.html" id="contato" class="menu-button">Contato</a>'
+    );
+
+//Variaveis para especificar a duracao do ms do "fadeIn()"
 var duracao = 250;
 var adicaoDuracao = 250;
 var duracaoFadeIn = 500;
 
-// Criacao dos menus em js, ficara mais facil caso tenha alguma alteracao nos botoes do menu
-
-// Index
-// prettier-ignore
-$("#index")
-    .append('<a href="./projeto/pages/page1-posts.html" id="posts" class="menu-button">Posts</a>')
-    .append('<a href="./projeto/pages/page2-projetos.html" id="projetos" class="menu-button">Projetos</a>')
-    .append('<a href="./projeto/pages/page3-contato.html" id="contato" class="menu-button">Contato</a>');
-
-//Restante da pagina
-// prettier-ignore
-$("#page")
-    .append('<a href="./page1-posts.html" id="posts" class="menu-button">Posts</a>')
-    .append('<a href="./page2-projetos.html" id="projetos" class="menu-button">Projetos</a>')
-    .append('<a href="./page3-contato.html" id="contato" class="menu-button">Contato</a>');
-
 // Fade in no conteudo do site quando entra ou vai para o outro link
-// prettier-ignore
 $(".title").hide().fadeIn(duracaoFadeIn);
 $(".description").hide().delay(250).fadeIn(duracaoFadeIn);
 
