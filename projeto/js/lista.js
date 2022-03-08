34c87e845cdfa36e965fa11b8a1200098c286a03
willshobwish <willianyos13@gmail.com>
2022-03-08 18:42:28 -0300

    Adição do prettier ignore

--- projeto/js/lista.js
+++ projeto/js/lista.js
@@ -12,14 +12,28 @@
 // 	"age": 29
 // }];
 
+// prettier-ignore
 let posts = [
     {
-        nome: "Como aumentar as vendas",
-        descricao:
+        "nome": "Como aumentar as vendas",
+        "descricao":
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia porta neque at sollicitudin. Vivamus id nibh ligula. Ut ullamcorper elit quis turpis condimentum euismod. Nunc blandit id ligula non volutpat. Mauris mollis tortor vitae purus tempor rhoncus. Cras semper, elit sed facilisis vulputate, tortor ligula sagittis elit, nec bibendum nisl massa non sapien. Nullam ligula enim, dictum vitae velit nec, consequat semper dolor. Mauris lacinia mi a dictum euismod. Donec maximus pretium venenatis. Suspendisse in lacinia nibh. Nullam imperdiet orci vel finibus sollicitudin. Morbi tincidunt egestas lectus quis suscipit. Morbi ultricies efficitur diam, at efficitur justo rhoncus quis. Sed semper sapien metus, vitae vehicula est gravida eleifend. Vivamus sollicitudin sit amet urna id varius. Morbi nisl sem, imperdiet quis pretium sed, mollis ac sapien.",
     },
 ];
 
-let contato;
+// prettier-ignore
+let contato = [
+    {
+        "nome": "",
+        "contato": "",
+        "email": "",
+    },
+];
 
-let sobre;
+// prettier-ignore
+let sobre = [
+    {
+        "nome": "",
+        "descricao": "",
+    },
+];
