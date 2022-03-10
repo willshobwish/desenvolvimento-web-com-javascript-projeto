17746fda696ec1e0dd4edd161816ebc8c46d0462
willshobwish <willianyos13@gmail.com>
2022-03-10 19:56:05 -0300

    Formatação e limpeza de código

--- projeto/js/goalsScriptPop.js
+++ projeto/js/goalsScriptPop.js
@@ -1,56 +1,52 @@
 window.onload = function () {
-    console.log('here')
-    var requestURL = 'projeto/js/db.json';
+    console.log("here");
+    var requestURL = "projeto/js/db.json";
     var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
+    request.open("GET", requestURL);
+    request.responseType = "json";
     request.send();
 
-    request.onload = function() {
+    request.onload = function () {
         var items = request.response.items;
         var listAbout = [];
         var listGoals = [];
         var listProjects = [];
 
-        let list = document.getElementById('goals-content');
-        console.log(list, 'list')
+        let list = document.getElementById("goals-content");
+        console.log(list, "list");
         items.forEach((element) => {
-            if(element.category == 1){
+            if (element.category == 1) {
                 listAbout.push(element);
-            } 
-            if(element.category == 2){
+            }
+            if (element.category == 2) {
                 listGoals.push(element);
             }
-            if(element.category == 3){
+            if (element.category == 3) {
                 listProjects.push(element);
             }
-        })
+        });
         listGoals.forEach((element) => {
-            const itemGoal = document.createElement('p');
-            const itemGoalTitle = document.createElement('h1');
+            const itemGoal = document.createElement("p");
+            const itemGoalTitle = document.createElement("h1");
 
-            const li = document.createElement('div');
-            li.classList.add('content-description');
+            const li = document.createElement("div");
+            li.classList.add("content-description");
             // li.classList.add('content-description');
             // li.classList.remove('list-style-type: none');
 
-            if(element.categoryLabel === 'title'){
-                itemGoalTitle.classList.add('content-description');
+            if (element.categoryLabel === "title") {
+                itemGoalTitle.classList.add("content-description");
                 itemGoalTitle.innerHTML = element.description;
                 li.appendChild(itemGoalTitle);
-
-            } else{
-                    itemGoal.classList.add('content-description');
-                    itemGoal.innerHTML = element.description;
-                    li.appendChild(itemGoal);
+            } else {
+                itemGoal.classList.add("content-description");
+                itemGoal.innerHTML = element.description;
+                li.appendChild(itemGoal);
             }
-        
-            list.appendChild(li);
-        })
-       
-       // item.classList.add('cat-' + element.category);
-       
-            
-    }
 
-}
+            list.appendChild(li);
+        });
+
+        // item.classList.add('cat-' + element.category);
+    };
+};
--- projeto/js/script.js
+++ projeto/js/script.js
@@ -1,77 +1,9 @@
-// window.onload = function () {
-//     var requestURL = 'projeto/js/db.json';
-//     var request = new XMLHttpRequest();
-//     request.open('GET', requestURL);
-//     request.responseType = 'json';
-//     request.send();
-
-//     request.onload = function() {
-//         var items = request.response.items;
-//         var listAbout = [];
-//         var listGoals = [];
-//         var listProjects = [];
-//         let list = document.querySelector('#content');
-//         items.forEach((element) => {
-//             if(element.category == 1){
-//                 listAbout.push(element);
-//             } 
-//             if(element.category == 2){
-//                 listGoals.push(element);
-//             }
-//             if(element.category == 3){
-//                 listProjects.push(element);
-//             }
-//         })
-//         listAbout.forEach((element) => {
-//             if(element.categoryLabel === 'title'){}
-//             var li = document.createElement('li');
-//             li.classList.add('content-description');
-
-//             var itemAbout = document.createElement('h1');
-//             itemAbout.classList.add('content-description');
-//             itemAbout.innerHTML = element.description;
-
-//             li.appendChild(itemAbout);
-//         })
-       
-//         list.appendChild(li);
-//        // item.classList.add('cat-' + element.category);
-       
-            
-//     }
-
-// }
-
 // A page can't be manipulated safely until the document is "ready." jQuery detects this state of readiness for you. https://learn.jquery.com/using-jquery-core/document-ready/
 $(document).ready(function () {
     console.log("ready!");
 });
 
-// Criacao dos menus em js, ficara mais facil caso tenha alguma alteracao nos botoes do menu, assim, alterara simultaneamente as quatro paginas.
-// Index
-$("#index") //Seleciona a id do div
-    .append(
-        '<a href="./projeto/pages/page1-posts.html" id="posts" class="menu-button">Posts</a>'
-    )
-    .append(
-        '<a href="./projeto/pages/page2-projetos.html" id="projetos" class="menu-button">Projetos</a>'
-    )
-    .append(
-        '<a href="./projeto/pages/page3-contato.html" id="contato" class="menu-button">Contato</a>'
-    );
-
-// Restante da pagina
-$("#page") //Seleciona a id do div
-    .append(
-        '<a href="./page1-posts.html" id="posts" class="menu-button">Posts</a>'
-    )
-    .append(
-        '<a href="./page2-projetos.html" id="projetos" class="menu-button">Projetos</a>'
-    )
-    .append(
-        '<a href="./page3-contato.html" id="contato" class="menu-button">Contato</a>'
-    );
-//
+//Funcao para adicionar fade in em elementos do HTML
 (function ($) {
     $.fn.fadeinfunction = function () {
         this.hide()
@@ -81,41 +13,12 @@ $("#page") //Seleciona a id do div
     };
 })(jQuery);
 
-//letiaveis para especificar a duracao do ms do "fadeIn()"
+//variavies para especificar a duracao do ms do "fadeIn()"
 let duracao = 250;
 let adicaoDuracao = 250;
 let duracaoFadeIn = 500;
 
-// Fade in nos botoes do menu, da esquerda para a direita
+//Funcao para executar o fade in
 $(".content-title").fadeinfunction();
 $(".content-description").fadeinfunction();
 $(".content-description-posts").fadeinfunction();
-/*
-//Posts
-var getFunction = $.getJSON(
-    "projeto/js/db.json",
-    function (data, textStatus, jqXHR) {
-        console.log(data)
-        console.log("Geração dinâmica das postagens funcional");
-        console.log(
-            "Quantidade de objetos dentro JSON: " +
-                Object.keys(data.dados["0"].data).length
-        );
-        console.log(data.dados["0"].data);
-        let tamanhoDaLista = Object.keys(data).length / 2 - 1;
-        let numberOfPost = Object.keys(data.dados).length / 2 - 1;
-        console.log(numberOfPost);
-        while (numberOfPost >= 0) {
-            let $posts = $("<div/>")
-                .attr("id", "post-" + numberOfPost)
-                .addClass("post-description-div")
-                .html(
-                    data.dados[numberOfPost].data +
-                        "<br>" +
-                        data.dados[numberOfPost].conteudo
-                );
-            $(".content-description-posts").append($posts);
-            numberOfPost -= 1;
-        }
-    }
-);*/
